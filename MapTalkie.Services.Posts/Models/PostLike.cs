75f6b9806a1595f2653913767ca2926baa382b4d
MaratBR <marat01q@gmail.com>
2021-11-30 08:07:08 +0700

    модели нам тут не нужны

--- MapTalkie.Services.Posts/Models/PostLike.cs
+++
@@ -1,8 +0,0 @@
-namespace MapTalkie.Services.Posts.Models
-{
-    public class PostLike
-    {
-        public string UserId { get; set; }
-        public long PostId { get; set; }
-    }
-}
--- MapTalkie.Services.Posts/Models/PostsDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace MapTalkie.Services.Posts.Models
-{
-    public class PostsDbContext : DbContext
-    {
-    }
-}
