5d89e1ff8e024cad5bf46ed98dddf7c17fe34a6b
MaratBR <marat01q@gmail.com>
2021-12-10 18:22:04 +0700

    добавление appsettings воркера в bin

--- MapTalkie.Services.Posts/Consumers/PostLikedConsumer/PostLikedConsumer.cs
+++ MapTalkie.Services.Posts/Consumers/PostLikedConsumer/PostLikedConsumer.cs
@@ -1,41 +1,35 @@
-using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MapTalkie.DB.Context;
 using MapTalkie.Domain.Messages.Posts;
 using MassTransit;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 
 namespace MapTalkie.Services.Posts.Consumers.PostLikedConsumer
 {
     public class PostLikedConsumer : IConsumer<Batch<PostEngagement>>
     {
-        private static DateTime? RefreshScheduledAt = null;
-        private static SemaphoreSlim RefreshSemaphore = new SemaphoreSlim(1, 1);
-        private readonly IMemoryCache _cache;
         private readonly AppDbContext _context;
         private readonly ILogger<PostLikedConsumer> _logger;
+        private long _updatesCount = 0;
 
         private long MaxUpdates = 100; // пока что пусть будет так
-        private long UpdatesCount = 0;
 
-        public PostLikedConsumer(AppDbContext context, IMemoryCache cache, ILogger<PostLikedConsumer> logger)
+        public PostLikedConsumer(AppDbContext context, ILogger<PostLikedConsumer> logger)
         {
             _context = context;
-            _cache = cache;
             _logger = logger;
         }
 
         public async Task Consume(ConsumeContext<Batch<PostEngagement>> context)
         {
             await PublishEngagement(context);
-            Interlocked.Add(ref UpdatesCount, context.Message.Length);
+            Interlocked.Add(ref _updatesCount, context.Message.Length);
 
-            if (Interlocked.Read(ref UpdatesCount) > MaxUpdates)
+            if (Interlocked.Read(ref _updatesCount) > MaxUpdates)
             {
-                Interlocked.And(ref UpdatesCount, 0);
+                Interlocked.And(ref _updatesCount, 0);
                 // тут может произойти race-condition, но я просто это проигнорирую,
                 // потому что это функция вызывается редко
                 await context.Send(new PostRankDecayRefresher.RefreshRankDecay(.0002, .25));
